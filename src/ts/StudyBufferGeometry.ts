e5d7ca7f21a180006d58e975c244dd12e151d7af
MasatoMakino <unagiinu.g@gmail.com>
2019-06-14 17:03:51 +0900

    BufferGeometry生成処理時間の比較を行う。

--- src/ts/StudyBufferGeometry.ts
+++ src/ts/StudyBufferGeometry.ts
@@ -1,25 +1,36 @@
-import {
-  Scene,
-  Mesh,
-  Fog,
-  PointLight,
-  PointLightHelper,
-  Color,
-  TorusGeometry,
-  AdditiveBlending
-} from "three";
+import { Scene, Mesh, Fog, PointLight, PointLightHelper, Color } from "three";
 import { Common } from "ts/Common";
-import { EarthGridMaterial } from "ts/earthGrid/EarthGridMaterial";
 import { SphereGeometry } from "three";
-import { DoubleSide } from "three";
 import { MeshLambertMaterial } from "three";
-import { BufferGeometry } from "three";
+import { SphereBufferGeometry } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { WebGLRenderer } from "three";
+import { Camera } from "three";
+import Stats from "stats.js";
+import * as dat from "dat.gui";
 
+/**
+ * BufferGeometryとGeometryの生成処理時間の比較を行う
+ */
 export class StudyBufferGeometry {
   public static readonly W = 640;
   public static readonly H = 480;
 
+  private mesh: Mesh;
+  private mat: MeshLambertMaterial;
+
+  private stats;
+  private geoStats;
+
+  private params: {
+    isBufferGeometry: boolean;
+    updateGeometry: boolean;
+  };
+
   constructor() {
+    this.initStats();
+    this.initGUI();
+
     const scene = Common.initScene();
     scene.fog = new Fog(0x000000, 80, 160);
     Common.initLight(scene);
@@ -34,29 +45,87 @@ export class StudyBufferGeometry {
     );
     const control = Common.initControl(camera, renderer);
     Common.initHelper(scene);
-    this.initObject(scene);
-    Common.render(control, renderer, scene, camera);
+
+    this.initPointLight(scene);
+    this.initMesh(scene);
+
+    this.refreshGeometry();
+    this.render(control, renderer, scene, camera);
   }
 
-  private initObject(scene: Scene): void {
+  private initStats(): void {
+    this.stats = new Stats();
+    this.stats.showPanel(1);
+    document.body.appendChild(this.stats.dom);
+    this.geoStats = this.stats.addPanel(
+      new Stats.Panel("ms, geo init", "#ff8", "#221")
+    );
+  }
+
+  private initGUI(): void {
+    this.params = {
+      isBufferGeometry: true,
+      updateGeometry: true
+    };
+    const gui = new dat.GUI();
+    gui.add(this.params, "isBufferGeometry").onChange(() => {
+      this.refreshGeometry();
+    });
+    gui.add(this.params, "updateGeometry").onChange(() => {
+      this.refreshGeometry();
+    });
+  }
+
+  private initPointLight(scene: Scene): void {
     const spot = new PointLight(0xffffff, 3, 0, 2);
     spot.position.set(10, 20, 30);
     scene.add(spot);
     const helper = new PointLightHelper(spot);
     scene.add(helper);
+  }
+
+  private initMesh(scene): void {
+    this.mat = new MeshLambertMaterial();
+    this.mat.color = new Color(0xff6666);
+    this.mesh = new Mesh();
+    this.mesh.material = this.mat;
+    scene.add(this.mesh);
+  }
+
+  private refreshGeometry(): void {
+    const now = performance.now();
 
     const seg = Math.pow(2, 8);
-    console.log(seg);
-    // const geo = new SphereGeometry(10, seg, seg);
-    const geo = new BufferGeometry().fromGeometry(
-      new SphereGeometry(10, seg, seg)
-    );
+    let geo;
+    if (this.params.isBufferGeometry) {
+      geo = new SphereBufferGeometry(10, seg, seg);
+    } else {
+      geo = new SphereGeometry(10, seg, seg);
+    }
+    this.mesh.geometry = geo;
 
-    const mat = new MeshLambertMaterial();
-    mat.color = new Color(0xff6666);
-    const mesh = new Mesh(geo, mat);
+    const onGeo = performance.now();
 
-    scene.add(mesh);
+    this.geoStats.update(onGeo - now, 1000);
+  }
+
+  public render(
+    control: OrbitControls,
+    renderer: WebGLRenderer,
+    scene: Scene,
+    camera: Camera
+  ) {
+    const rendering = () => {
+      this.stats.begin();
+      control.update();
+      if (this.params.updateGeometry) {
+        this.refreshGeometry();
+      }
+      renderer.render(scene, camera);
+      this.stats.end();
+      requestAnimationFrame(rendering);
+    };
+    rendering();
   }
 }
 
