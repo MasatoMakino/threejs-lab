f5a42b206ab04206421e2f43df45e4a4a3b10536
MasatoMakino <unagiinu.g@gmail.com>
2019-06-28 18:02:21 +0900

    未使用プロパティ削除

--- src/ts/customPhongMaterial/IMaskable.ts
+++ src/ts/customPhongMaterial/IMaskable.ts
@@ -1,13 +1,8 @@
 import { Texture } from "three";
-import { IUniform } from "three";
 import { MaterialInterfaceChunk } from "ts/customPhongMaterial/CustomPhongMaterial";
 
 export interface IMaskable {
-  uniforms: { [uniform: string]: IUniform };
   maskTexture: Texture;
-  // isLoading: boolean;
-  // loadMaskTexture(url: string): void;
-  // deleteMaskTexture(): void;
 }
 
 export class MaskableMaterialChunk extends MaterialInterfaceChunk {
