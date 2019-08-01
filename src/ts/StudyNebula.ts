11a8cd617a74a7575037068cbc06ffa5aba0af98
MasatoMakino <unagiinu.g@gmail.com>
2019-08-01 13:04:36 +0900

    NebulaデモのGUIを共通化したものに入れ替え。

--- src/ts/StudyNebula.ts
+++ src/ts/StudyNebula.ts
@@ -1,24 +1,25 @@
 import { Common } from "ts/Common";
-import { Scene, PlaneGeometry, Mesh, Color } from "three";
+import { Color, Scene } from "three";
+import * as THREE from "three";
 
 import System, {
-  Emitter,
-  Rate,
-  Span,
-  Position,
-  Mass,
-  Radius,
-  Life,
-  PointZone,
-  Vector3D,
   Alpha,
+  Color as NebulaColor,
+  Emitter,
+  Life,
+  Mass,
+  PointZone,
+  Position,
+  RadialVelocity,
+  Radius,
+  Rate,
   Scale,
-  Color as NeburaColor,
+  Span,
   SpriteRenderer,
-  RadialVelocity
+  Vector3D
 } from "three-nebula";
-import * as THREE from "three";
 import * as dat from "dat.gui";
+import { NebulaGUI } from "ts/NebulaGUI";
 
 export class Study {
   public static readonly W = 640;
@@ -31,7 +32,7 @@ export class Study {
   private life: Life;
   private alpha: Alpha;
   private scale: Scale;
-  private color: NeburaColor;
+  private color: NebulaColor;
 
   constructor() {
     const scene = Common.initScene();
@@ -61,7 +62,7 @@ export class Study {
 
     this.alpha = new Alpha(1, 0);
     this.scale = new Scale(0.1, 1.0);
-    this.color = new NeburaColor(new Color(0x00ff00), new Color(0x00ff00));
+    this.color = new NebulaColor(new Color(0x00ff00), new Color(0x00ff00));
 
     this.emitter
       .setRate(new Rate(new Span(4, 16), 0.1))
@@ -83,219 +84,17 @@ export class Study {
 
     const folder = gui.addFolder("Emitter");
     folder.open();
-    this.initGUIEmitter(folder);
-    this.initGUIRate(folder);
-    this.initGUIRange(folder);
-    this.initGUIRadius(folder);
-    this.initGUILife(folder);
+    NebulaGUI.initEmitterPosition(folder, [this.emitter]);
+    NebulaGUI.initEmitterRate(folder, [this.emitter]);
+    NebulaGUI.initRange(folder, [this.range]);
+    NebulaGUI.initRadius(folder, [this.radius]);
+    NebulaGUI.initLife(folder, [this.life]);
 
     const folderBehaviour = gui.addFolder("Behaviour");
-    this.initGUIAlpha(folderBehaviour);
-    this.initGUIScale(folderBehaviour);
-    this.initGUIColor(folderBehaviour);
+    NebulaGUI.initAlpha(folderBehaviour, [this.alpha]);
+    NebulaGUI.initScale(folderBehaviour, [this.scale]);
+    NebulaGUI.initColor(folderBehaviour, [this.color]);
     folderBehaviour.open();
-  }
-
-  /**
-   * emitterの座標制御
-   * @param gui
-   */
-  private initGUIEmitter(gui): void {
-    const folder = gui.addFolder("Position");
-    folder.add(this.emitter.position, "x", -30.0, 30.0);
-    folder.add(this.emitter.position, "y", -30.0, 30.0);
-    folder.add(this.emitter.position, "z", -30.0, 30.0);
-    folder.open();
-  }
-
-  /**
-   * パーティクル生成間隔の制御パネル
-   * @param gui
-   */
-  private initGUIRate(gui): void {
-    //spanクラスはmin, maxの２つの値をセットにした範囲を指定する型
-    const prop = {
-      numPanA: this.emitter.rate.numPan.a,
-      numPanB: this.emitter.rate.numPan.b,
-      timePan: this.emitter.rate.timePan.a
-    };
-
-    /**
-     * 1回で射出されるパーティクルの数, min, max
-     */
-    const folder = gui.addFolder("Rate");
-    folder
-      .add(prop, "numPanA", 0.0, 30.0)
-      .step(1)
-      .onChange(val => {
-        this.emitter.rate.numPan.a = val;
-      });
-    folder
-      .add(prop, "numPanB", 0.0, 30.0)
-      .step(1)
-      .onChange(val => {
-        this.emitter.rate.numPan.b = val;
-      });
-    folder
-      .add(prop, "timePan", 0.01, 0.5)
-      .step(0.01)
-      .onChange(val => {
-        this.emitter.rate.timePan.a = val;
-      });
-    folder.open();
-  }
-
-  /**
-   * 射出した粒子の到達点および射出角度を制御するパネル
-   * @param gui
-   */
-  private initGUIRange(gui): void {
-    //spanクラスはmin, maxの２つの値をセットにした範囲を指定する型
-    const prop = {
-      radiusMin: this.range.radiusPan.a,
-      radiusMax: this.range.radiusPan.b,
-      tha: this.range.tha
-    };
-
-    const folder = gui.addFolder("RadialVelocity");
-    folder
-      .add(prop, "radiusMin", 0.0, 45.0)
-      .step(0.1)
-      .onChange(val => {
-        this.range.radiusPan.a = val;
-      });
-    folder
-      .add(prop, "radiusMax", 0.0, 45.0)
-      .step(0.1)
-      .onChange(val => {
-        this.range.radiusPan.b = val;
-      });
-    folder
-      .add(prop, "tha", 0.0, Math.PI)
-      .step(0.1)
-      .onChange(val => {
-        this.range.tha = val;
-      });
-    folder.open();
-  }
-
-  /**
-   * Spriteのサイズを制御するパネル
-   * @param gui
-   */
-  private initGUIRadius(gui): void {
-    //spanクラスはmin, maxの２つの値をセットにした範囲を指定する型
-    const prop = {
-      min: this.radius.radius.a,
-      max: this.radius.radius.b
-    };
-
-    const folder = gui.addFolder("Initial Sprite Size");
-    folder
-      .add(prop, "min", 0.0, 45.0)
-      .step(0.1)
-      .onChange(val => {
-        this.radius.radius.a = val;
-      });
-    folder
-      .add(prop, "max", 0.0, 45.0)
-      .step(0.1)
-      .onChange(val => {
-        this.radius.radius.b = val;
-      });
-    folder.open();
-  }
-
-  /**
-   * Spriteのサイズを制御するパネル
-   * @param gui
-   */
-  private initGUILife(gui): void {
-    //spanクラスはmin, maxの２つの値をセットにした範囲を指定する型
-    const span: Span = this.life.lifePan;
-    const prop = {
-      min: span.a,
-      max: span.b
-    };
-
-    const folder = gui.addFolder("Life");
-    folder
-      .add(prop, "min", 0.0, 12.0)
-      .step(0.1)
-      .onChange(val => {
-        span.a = val;
-      });
-    folder
-      .add(prop, "max", 0.0, 24.0)
-      .step(0.1)
-      .onChange(val => {
-        span.b = val;
-      });
-    folder.open();
-  }
-
-  private initGUIAlpha(gui): void {
-    //spanクラスはmin, maxの２つの値をセットにした範囲を指定する型
-    const prop = {
-      start: this.alpha.alphaA.a,
-      end: this.alpha.alphaB.a
-    };
-
-    const folder = gui.addFolder("Alpha");
-    folder
-      .add(prop, "start", 0.0, 1.0)
-      .step(0.1)
-      .onChange(val => {
-        this.alpha.alphaA.a = val;
-      });
-    folder
-      .add(prop, "end", 0.0, 1.0)
-      .step(0.1)
-      .onChange(val => {
-        this.alpha.alphaB.a = val;
-      });
-    folder.open();
-  }
-
-  private initGUIScale(gui): void {
-    const prop = {
-      start: this.scale.scaleA.a,
-      end: this.scale.scaleB.a
-    };
-    const folder = gui.addFolder("Scale");
-    folder
-      .add(prop, "start", 0.0, 3.0)
-      .step(0.1)
-      .onChange(val => {
-        this.scale.scaleA.a = val;
-      });
-    folder
-      .add(prop, "end", 0.0, 3.0)
-      .step(0.1)
-      .onChange(val => {
-        this.scale.scaleB.a = val;
-      });
-    folder.open();
-  }
-
-  private initGUIColor(gui): void {
-    const col = this.color;
-    const prop = {
-      start: col.colorA.colors[0].getHex(),
-      end: col.colorB.colors[0].getHex()
-    };
-
-    const onChange = (colorPan, val) => {
-      colorPan.colors[0].setHex(val);
-    };
-    const folder = gui.addFolder("Color");
-    folder.addColor(prop, "start").onChange(val => {
-      onChange(col.colorA, val);
-    });
-    folder.addColor(prop, "end").onChange(val => {
-      onChange(col.colorB, val);
-    });
-    folder.open();
   }
 }
 
