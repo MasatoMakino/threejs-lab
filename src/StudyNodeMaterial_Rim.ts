45fde8dbb1217f4bc2efe499b583f92fe0cc0178
MasatoMakino <makino.masato.g@gmail.com>
2024-06-08 14:54:03 +0900

    add : gui for rim effect

--- src/StudyNodeMaterial_Rim.ts
+++ src/StudyNodeMaterial_Rim.ts
@@ -1,11 +1,16 @@
-import { Mesh, Scene, Color, TorusGeometry } from "three";
+import { Mesh, Scene, Color, TorusGeometry, Uniform } from "three";
 import {
   MeshBasicNodeMaterial,
   color,
   uniform,
+  ShaderNodeObject,
+  UniformNode,
 } from "three/examples/jsm/nodes/Nodes.js";
 import { Common } from "./Common";
 import { rimEffect } from "./tsl/RimFunction.js";
+import GUI from "lil-gui";
+
+type UniformType<T> = ShaderNodeObject<UniformNode<T>>;
 
 export class Study {
   public static readonly W = 640;
@@ -48,7 +53,61 @@ export class Study {
     );
     const mesh = new Mesh(geo, mat);
     scene.add(mesh);
+
+    this.initGUI(
+      colorUniform,
+      rimColor,
+      rimStrength,
+      rimPow,
+      rimInnerColor,
+      rimInnerStrength,
+      rimInnerPow
+    );
   }
+
+  initGUI = (
+    color: UniformType<Color>,
+    rimColor: UniformType<Color>,
+    rimStrength: UniformType<number>,
+    rimPow: UniformType<number>,
+    rimInnerColor: UniformType<Color>,
+    rimInnerStrength: UniformType<number>,
+    rimInnerPow: UniformType<number>
+  ): void => {
+    const gui = new GUI();
+    const options = {
+      color: color.value.getHex(),
+      rimColor: rimColor.value.getHex(),
+      rimStrength: rimStrength.value,
+      rimPow: rimPow.value,
+      rimInnerColor: rimInnerColor.value.getHex(),
+      rimInnerStrength: rimInnerStrength.value,
+      rimInnerPow: rimInnerPow.value,
+    };
+    gui.addColor(options, "color").onChange((v) => {
+      color.value.setHex(v);
+    });
+    const rimFolder = gui.addFolder("Rim");
+    rimFolder.addColor(options, "rimColor").onChange((v) => {
+      rimColor.value.setHex(v);
+    });
+    rimFolder.add(options, "rimStrength", 0, 1).onChange((v) => {
+      rimStrength.value = v;
+    });
+    rimFolder.add(options, "rimPow", 0, 5).onChange((v) => {
+      rimPow.value = v;
+    });
+    const rimInnerFolder = gui.addFolder("Rim Inner");
+    rimInnerFolder.addColor(options, "rimInnerColor").onChange((v) => {
+      rimInnerColor.value.setHex(v);
+    });
+    rimInnerFolder.add(options, "rimInnerStrength", 0, 1).onChange((v) => {
+      rimInnerStrength.value = v;
+    });
+    rimInnerFolder.add(options, "rimInnerPow", 0, 5).onChange((v) => {
+      rimInnerPow.value = v;
+    });
+  };
 }
 
 window.onload = () => {
